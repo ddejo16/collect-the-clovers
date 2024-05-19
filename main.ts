806d7863eecfbdeb9d84c4229bdd7f2b60c5e171
ddejo16 <ddejo16@eq.edu.au>
2024-05-19 16:16:50 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,5 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.disintegrate, 100)
+    music.pewPew.play()
+    info.changeLifeBy(-1)
+})
